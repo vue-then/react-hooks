d0cc2c35dd05705d039693cc9c46d597015e991c
yyccQQu <643929860@qq.com>
2019-07-30 21:49:59 +0700

    use state的使用

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# react-hooks
+### react-hooks
 - some of usehooks
 - nvm use v10.15.3
 - npx create-react-app projectName
 - 此工程为MPA
 
-# react新特性
+### react新特性
 - Context
 - ![sql](https://github.com/vue-then/react-hooks/blob/master/img/1.png)
 - ContextType
@@ -12,6 +12,24 @@
 - Suspense
 - memo
 
+### 类组件不足（一）
+1，状态逻辑复用难
+- 缺少复用机制
+- 渲染属性和高阶组件导致层级冗余
+2，趋向复杂难以维护
+- 生命周期函数混杂不相干逻辑
+- 相干逻辑分散在不同生命周期
+3，this指向困扰
+- 内联函数过度创建新句柄
+- 类成员函数不能保证this
+### Hooks优势
+优化类组件的三大问题
+- 函数组件无this问题
+- 自定义Hook方便复用状态逻辑
+- 副作用的关注点分离
 
+
+### 注意
+- reacthooks必须按照顺序调用
 
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,45 @@
-import React, { Component, PureComponent, memo } from "react";
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, useState } from "react";
 
-// class Foo extends PureComponent {
-// 	// PureComponent: 只有传入的props第一级发生变化才会触发此函数
-// 	// shouldComponentUpdate(nextProps, nextState) {
-// 	// 	if (nextProps.name === this.props.name) {
-// 	// 		return false;
-// 	// 	}
-// 	// 	return true;
-// 	// }
+// class App extends Component {
 // 	render() {
-// 		console.log("Foo render");
-// 		return <div>{this.props.person.age}</div>;
-// 	}
+//     return null;
+//   }
 // }
-const Foo = memo(function Foo(props) {
-  return <div>{props.person.age}</div>;
-})
 
-class App extends Component {
-	state = {
-		count: 0,
-		person: {
-			age: 1
-		}
-	};
-	// callback (){}; //this指向不确定
-	callback = () => {};
-
+class App2 extends Component {
+  state = {
+    count: 0,
+  }
 	render() {
-		const person = this.state.person;
-		return (
-			<div>
-				{/* <button onClick={() => this.setState({count: this.state.count+=1})}>Add</button> */}
-				<button
-					onClick={() => {
-						person.age++;
-						this.setState({
-							person
-						});
-					}}
-				>
-					Add
-				</button>
-
-				<Foo person={person} cb={this.callback} />
-			</div>
-		);
+    const {count} = this.state;
+    return (
+      <button
+      type="button"
+      onClick={()=>{this.setState({ count: count+1 });}}>
+        Click {count}
+      </button>
+    )
+		
 	}
 }
+
+function App(prop) {
+	// const [count, setCount] = useState(0)
+	const [count, setCount] = useState(() => {
+		console.log("initial count");
+		return prop.defaultCount || 0;
+	});
+
+	return (
+		<button type="button" onClick={() => setCount(count + 1)}>
+			Click {count}
+		</button>
+	);
+}
+
+
+
+
 
 
 export default App;
