b8e323951f10ca3d18ef57ba88b1c5475655c4fa
yyccQQu <643929860@qq.com>
2019-07-30 21:08:41 +0700

    memo实现指定组件进行渲染

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,51 @@
-import React, { Component, lazy, Suspense } from "react";
+import React, { Component, PureComponent, memo } from "react";
 import logo from './logo.svg';
 import './App.css';
 
-const About = lazy(()=>import(/* webpackChunkName: "about" */ './About.jsx'));
-
-// ErrorBoundary
-// componentDidCatch
+// class Foo extends PureComponent {
+// 	// PureComponent: 只有传入的props第一级发生变化才会触发此函数
+// 	// shouldComponentUpdate(nextProps, nextState) {
+// 	// 	if (nextProps.name === this.props.name) {
+// 	// 		return false;
+// 	// 	}
+// 	// 	return true;
+// 	// }
+// 	render() {
+// 		console.log("Foo render");
+// 		return <div>{this.props.person.age}</div>;
+// 	}
+// }
+const Foo = memo(function Foo(props) {
+  return <div>{props.person.age}</div>;
+})
 
 class App extends Component {
 	state = {
-		hasError: false
-	};
-	// componentDidCatch() {
-	//   this.setState({
-	//     hasError: true,
-	//   });
-	// }
-	static getDerivedStateFromError() {
-		return {
-			hasError: true
-		};
-	}
-	render() {
-		if(this.state.hasError){
-		  return <div>error</div>
+		count: 0,
+		person: {
+			age: 1
 		}
+	};
+	// callback (){}; //this指向不确定
+	callback = () => {};
+
+	render() {
+		const person = this.state.person;
 		return (
 			<div>
-				<Suspense fallback={<div>loading</div>}>
-					<About />
-				</Suspense>
+				{/* <button onClick={() => this.setState({count: this.state.count+=1})}>Add</button> */}
+				<button
+					onClick={() => {
+						person.age++;
+						this.setState({
+							person
+						});
+					}}
+				>
+					Add
+				</button>
+
+				<Foo person={person} cb={this.callback} />
 			</div>
 		);
 	}
