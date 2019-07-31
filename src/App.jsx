028bb23bfddc2a6f39278fb12c85618d292819bf
yyccQQu <643929860@qq.com>
2019-07-31 21:31:11 +0700

    useEffect的使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState } from "react";
+import React, { Component, useState, useEffect } from "react";
 
 // class App extends Component {
 // 	render() {
@@ -7,33 +7,109 @@ import React, { Component, useState } from "react";
 // }
 
 class App2 extends Component {
-  state = {
-    count: 0,
-  }
+	state = {
+		count: 0,
+		size: {
+			width: document.documentElement.clientWidth,
+			height: document.documentElement.clientHeight
+		}
+	};
+
+	// @bind()
+	// onResize(){}
+
+	onResize = () => {
+		this.setState({
+			size: {
+				width: document.documentElement.clientWidth,
+				height: document.documentElement.clientHeight
+			}
+		});
+	};
+	componentDidMount() {
+		document.title = this.state.count;
+
+		window.addEventListener("resize", this.onResize, false);
+	}
+	componentWillUnmount() {
+		window.removeEventListener("resize", this.onResize, false);
+	}
+	componentDidUpdate() {
+		document.title = this.state.count;
+	}
 	render() {
-    const {count} = this.state;
-    return (
-      <button
-      type="button"
-      onClick={()=>{this.setState({ count: count+1 });}}>
+		const { count,size } = this.state;
+		return (
+			<button
+				type="button"
+				onClick={() => {
+					this.setState({ count: count + 1 });
+				}}
+			>
         Click {count}
-      </button>
-    )
-		
+        size: {size.width}x{size.height}
+			</button>
+		);
 	}
 }
 
 function App(prop) {
-	// const [count, setCount] = useState(0)
-	const [count, setCount] = useState(() => {
-		console.log("initial count");
-		return prop.defaultCount || 0;
-	});
+  const [count, setCount] = useState(0);
+  const [size, setSize] = useState({
+		width: document.documentElement.clientWidth,
+		height: document.documentElement.clientHeight
+  });
+  const onResize = () => {
+    setSize({
+      width: document.documentElement.clientWidth,
+      height: document.documentElement.clientHeight
+	  });
+  }
+
+	useEffect(() => {
+    document.title = count;
+  })
+  useEffect(() => {
+      // 一直在调用
+      window.addEventListener("resize", onResize, false);
+      // 触发时机 重新渲染，组件卸载、
+      // 第一次渲染后，组件卸载之前
+      return () => {
+        window.removeEventListener("resize", onResize, false);
+      };
+    }, 
+  // 可选数组，只有数组的每一项不变，useEffect才不会执行
+  []);
+
+  // 测试count变化是否才不发useEffect函数
+  useEffect(() => {
+    console.log('count', count)
+  }, [count])
+
+  const onClick = () => {
+    console.log('click');
+  }
+  useEffect(() => {
+    document.querySelector("#size").addEventListener('click', onClick, false);
+
+    return () => {
+      document.querySelector("#size").removeEventListener("resize", onResize, false);
+    }
+  })
+
 
 	return (
-		<button type="button" onClick={() => setCount(count + 1)}>
-			Click {count}
-		</button>
+		<div>
+			<button type="button" onClick={() => setCount(count + 1)}>
+				Click {count}
+				size: {size.width}x{size.height}
+			</button>
+      {
+        count%2
+        ?<span id="size">size: {size.width}x{size.height}</span>
+        :<p id="size">size: {size.width}x{size.height}</p>
+      }
+		</div>
 	);
 }
 
