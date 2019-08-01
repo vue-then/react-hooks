cc338b8233e36006bc2d4f0b5f1993937c35274a
js103 <643929860@qq.com>
2019-08-01 08:51:51 +0700

    memo 和 callback hooks的使用

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component,useState,useMemo,useCallback,memo  } from "react";
 
 // class App extends Component {
 // 	render() {
@@ -6,109 +6,38 @@ import React, { Component, useState, useEffect } from "react";
 //   }
 // }
 
-class App2 extends Component {
-	state = {
-		count: 0,
-		size: {
-			width: document.documentElement.clientWidth,
-			height: document.documentElement.clientHeight
-		}
-	};
-
-	// @bind()
-	// onResize(){}
-
-	onResize = () => {
-		this.setState({
-			size: {
-				width: document.documentElement.clientWidth,
-				height: document.documentElement.clientHeight
-			}
-		});
-	};
-	componentDidMount() {
-		document.title = this.state.count;
-
-		window.addEventListener("resize", this.onResize, false);
-	}
-	componentWillUnmount() {
-		window.removeEventListener("resize", this.onResize, false);
-	}
-	componentDidUpdate() {
-		document.title = this.state.count;
-	}
-	render() {
-		const { count,size } = this.state;
-		return (
-			<button
-				type="button"
-				onClick={() => {
-					this.setState({ count: count + 1 });
-				}}
-			>
-        Click {count}
-        size: {size.width}x{size.height}
-			</button>
-		);
-	}
-}
+const Counter = memo(function Counter(props) {
+    console.log('Counter render');
+    return (
+        <h1 onClick={props.onClick}>{props.count}</h1>
+    )
+})
 
 function App(prop) {
   const [count, setCount] = useState(0);
-  const [size, setSize] = useState({
-		width: document.documentElement.clientWidth,
-		height: document.documentElement.clientHeight
-  });
-  const onResize = () => {
-    setSize({
-      width: document.documentElement.clientWidth,
-      height: document.documentElement.clientHeight
-	  });
-  }
-
-	useEffect(() => {
-    document.title = count;
-  })
-  useEffect(() => {
-      // 一直在调用
-      window.addEventListener("resize", onResize, false);
-      // 触发时机 重新渲染，组件卸载、
-      // 第一次渲染后，组件卸载之前
-      return () => {
-        window.removeEventListener("resize", onResize, false);
-      };
-    }, 
-  // 可选数组，只有数组的每一项不变，useEffect才不会执行
-  []);
-
-  // 测试count变化是否才不发useEffect函数
-  useEffect(() => {
-    console.log('count', count)
-  }, [count])
-
-  const onClick = () => {
-    console.log('click');
-  }
-  useEffect(() => {
-    document.querySelector("#size").addEventListener('click', onClick, false);
-
-    return () => {
-      document.querySelector("#size").removeEventListener("resize", onResize, false);
-    }
-  })
-
+  const [clickCount, setClickCount] = useState(0);
+  // 返回值可以直接参与渲染，渲染期间完成的,策略和useEffect一样的
+  const double = useMemo(() => {
+		return count * 2;
+    }, [count===3]);
+  
+    // const onClick = useMemo(() => {
+    //     return () => {
+    //         console.log('Click');
+    //     }
+    // }, [])
+    const onClick = useCallback(() => {
+        console.log('Click')
+        setClickCount((clickCount) => clickCount+1)
+    },[])
+    // useMemo(()=>fn) === useCallback(fn)
 
 	return (
 		<div>
 			<button type="button" onClick={() => setCount(count + 1)}>
-				Click {count}
-				size: {size.width}x{size.height}
+				Click ({count}) ，double：({double})
 			</button>
-      {
-        count%2
-        ?<span id="size">size: {size.width}x{size.height}</span>
-        :<p id="size">size: {size.width}x{size.height}</p>
-      }
+            <Counter count={double} onClick={onClick}/>
 		</div>
 	);
 }
