51092968553f9ed31031d7549b304f466e280f20
js103 <643929860@qq.com>
2019-08-09 09:12:48 +0700

    1

--- src/ticket/Candidate.jsx
+++ src/ticket/Candidate.jsx
@@ -1,13 +1,13 @@
 import React, { memo, useState, useCallback, useMemo, useReducer } from "react";
 import PropTypes from "prop-types";
 import classnames from "classnames";
-import './Detail.css'
+import './Candidate.css'
 
-export default function Detail(props) {
+export default function Candidate(props) {
     return (
-        <div className="detail">detail</div>
+        <div className="candidate">candidate</div>
     )
 }
-Detail.propTypes = {
+Candidate.propTypes = {
 
 }
